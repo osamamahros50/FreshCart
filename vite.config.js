6b2bcf16e269ad77f43a4ea5ee7b843aac15a713
ussoooo <osamamahros50@gmail.com>
2025-07-29 03:46:25 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    resolve: {
+  base: '/FreshCart/', // 👈 هنا صح، خارج resolve
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-     base: "/FreshCart/", 
   },
 })
